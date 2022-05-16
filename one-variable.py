7b1017fdb82e94ed6bccfbdd0bb0df56c011fc55
Yehor Panchenko <panchenko.yehor@huawei.com>
2022-05-16 20:42:53 +0300

    One variable file refactoring

--- one-variable.py
+++ one-variable.py
@@ -1,12 +1,14 @@
 from multiprocessing import Pool
 import time
 
+
 def delaysec(x):
     time.sleep(x)
 
+
 if __name__ == "__main__":
     start = time.time()
-    with Pool(5) as p:
-        #p.apply(delaysec)
-        p.map(delaysec, [1, 1, 1, 1, 1])
+    num_workers = 32
+    with Pool(num_workers) as p:
+        p.map(delaysec, [1] * num_workers)
     print(time.time() - start)
